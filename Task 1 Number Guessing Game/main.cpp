67cc8ad8f7f0992f8eddca5670ca08d88be719eb
Muhammad Haroon <mharoon1326@gmail.com>
2023-10-04 21:41:54 +0500

    Number Guessing Game
    Random number between 1-100 is generated and you have to guess the game.

---
+++ Task 1 Number Guessing Game/main.cpp
@@ -0,0 +1,58 @@
+// #include <iostream>
+// #include <cstdlib>
+// using namespace std;
+// int main() 
+// {
+//     int randomNum = rand();
+//     int gnumber;
+//     cout<<"Guess The Number !";
+//     cin>>gnumber;
+//     while(gnumber!=randomNum)
+//     {
+//         if(gnumber>randomNum)
+//         {
+//             cout<<"High\n";
+//         }
+//         else if(gnumber<randomNum)
+//         {
+//             cout<<"Low\n";
+//         }
+//     }
+//     if(gnumber==randomNum)
+//     {
+//         cout<<"BINGO !\nYou Guessed It Right !";
+//     }
+//     return 0;
+// }
+#include <iostream>
+#include <cstdlib>
+#include <ctime> // Include this header for srand()
+
+using namespace std;
+
+int main() 
+{
+    srand(time(0)); // Seed the random number generator with the current time
+    int randomNum=rand()%100 + 1; // Generates a random number between 1 and 100
+    int gnumber;
+    cout<<"\nGuess The Number (between 1 and 100)!\n";
+    do
+    {
+        cin>>gnumber;
+        if(gnumber>randomNum)
+        {
+            cout<<"High\n";
+        }
+        else if(gnumber<randomNum)
+        {
+            cout<<"Low\n";
+        }
+        else
+        {
+            cout<<"BINGO!\nYou Guessed It Right ! "<<endl;
+        }
+    }
+    while(gnumber!=randomNum);
+
+    return 0;
+}
